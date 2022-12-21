de3ddc704c70badbaa4f817e9b14bf33ff6a7b2d
unknown <abhimvj575@gmail.com>
2022-12-21 10:03:06 +0530

    [Abhilash] Added: enter a valid First Name

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name");
+            var FirstName=Console.ReadLine();
+            var pattern = "^[A-Z]{1}[a-z]{2,}$";
+            Regex regex= new Regex(pattern);
+            if( regex.IsMatch(FirstName))
+            {
+                Console.WriteLine(FirstName + " Valid Firstname ");
+            }
+            else
+            {
+                Console.WriteLine(FirstName + " Invalid Firstname");
+                
+            }
+
+        }
+    }
+}
