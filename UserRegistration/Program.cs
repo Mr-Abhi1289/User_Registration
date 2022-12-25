2f1af751469e75066187e3e8b0842746c36031f5
unknown <abhimvj575@gmail.com>
2022-12-25 20:45:47 +0530

    [Abhilash] Added :validating firstname with given pattern

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInfo user = new UserInfo();
+            Console.WriteLine("Enter first Name");
+            string firstname = Console.ReadLine();
+            Console.WriteLine(user.validateFirstName(firstname));
+        }
+    }
+}
